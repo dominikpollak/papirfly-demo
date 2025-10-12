80240306e40302a8d27454bee5f443cf642b09de
Dominik Pollák <dominikpollak17@gmail.com>
2025-10-12 15:27:34 +0200

    tile text padding

--- src/components/GridSection.tsx
+++ src/components/GridSection.tsx
@@ -43,7 +43,7 @@ export const GridSection = () => {
                       : g.color,
                   }}
                 >
-                  {g.text}
+                  <span>{g.text}</span>
                 </a>
               );
             })}
